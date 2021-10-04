b5b9076765c3b8f38135e6c7c39fc3dd3e280033
mjc715 <mjc715@miami.edu>
2021-10-04 15:42:43 -0400

    v3 very buggy

--- src/dentistry.java
+++ src/dentistry.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
-//============================================================================
+//======================================================================================================================
 public class dentistry {
-    //----------------------------------------------------------------------------
+    //------------------------------------------------------------------------------------------------------------------
     private static final Scanner keyboard = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        String teeth = "";
-        int familyMembers, index, findex;
+        String uppers = "";
+        int familyMembers, i, j;
         int letterCount = 0;
         char[][][] teethArray;
         String[] namesArray;
+        index = 0;
+        findex = 0;
 
         System.out.println("Welcome to the Floridian Tooth Records");
         System.out.println("======================================");
@@ -25,34 +27,38 @@ public class dentistry {
         teethArray = new char[familyMembers][2][10];
 
 
-        do {
+        for (i = 0; i < familyMembers; i++) {
             System.out.printf("%-50s :", "Please enter family member name");
-            namesArray[index] = keyboard.next();
-            do {
-                for (index = 0; index < familyMembers; index++) {
-
-
-                    System.out.printf("%-50s :", "Please enter uppers for " + namesArray[index]);
-                    teeth = keyboard.next();
-                    letterCount = teeth.length();
-                    if (letterCount > 10) {
-                        System.out.printf("%-50s \n1", "Too many teeth, try again");
-                    }
-                }
-            } while (letterCount > 10);
+            namesArray[i] = keyboard.next();
+            uppers = getUppers(i, namesArray);
 
 
 
-                for (findex = 0; findex < teeth.length(); findex++) {
-                    teeth = teeth.toUpperCase();
-                    teethArray[index][0][findex] = teeth.charAt(findex);
-
-                }
-
-
-            } while (index < familyMembers);
         }
     }
+//----------------------------------------------------------------------------------------------------------------------
+    private static String getUppers(int i, String [] namesArray) {
+
+        String uppers;
+        int teethCount;
+        char forbiddenChars = {'A','D','E','F','G','H','I','J','K','L','N','O','P','Q','R','S','T','U','V','W','X','Y',
+        'Z'};
+
+        System.out.printf("%-50s :", "Please enter uppers for " + namesArray[i]);
+        uppers = keyboard.next();
+        teethCount = uppers.length();
+
+        while (teethCount > 10) {
+            System.out.printf("%-50s \n", "Too many teeth, try again: ");
+            uppers = keyboard.next();
+            teethCount = uppers.length();
+        }
+        while (uppers.contains('a'));
+
+    }
+}
+
+
 
 
 
